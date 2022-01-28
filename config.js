474582fd7dfbb200c16b17a7cb6af627e3de75f3
Laras <96417338+Larasatilaras@users.noreply.github.com>
2022-01-28 17:07:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 global.linkGC = ['https://chat.whatsapp.com/EVCGfzxLWfp81n0WhmVklH', 'https://chat.whatsapp.com/FnNAbem8o6r4pgLhSdO8Q9', 'https://chat.whatsapp.com/HBhy7rVae3o0PkIfkhvs2N'] // ganti jadi group lu
-global.owner = ['62858929626673', '994409869765', '6285892962667'] // Put your number here
+global.owner = ['6285759662798', '994409869765', '6285759662798'] // Put your number here
 global.mods = ['6289669349047', '6288261394557'] // Want some help?
-global.prems = ['62858929626673','994401240133','994409869765','6285892962667'] // Premium user has unlimited limit
+global.prems = ['6285759662798','994401240133','994409869765','6285759662798'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
@@ -24,8 +24,8 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'by Raditya' // ganti aja
-global.author = 'RadBotZ' // ganti aja
+global.packname = 'by Laras' // ganti aja
+global.author = 'RasBotZ' // ganti aja
 
 // silakan di ganti klo mau
 global.wait = '_*tunggu..*_'
@@ -43,7 +43,7 @@ global.ttq = `Thanks to:
 •IrwanX
 •Dawnfrosty
 •Beniismael
-•Raditya (owner)
+•Laras (owner)
 •Rasel  
 •Nadin
 •Aca Mirabel
